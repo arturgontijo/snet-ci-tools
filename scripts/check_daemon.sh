99afa984aafc33da0f8b49bf6f542f97fa4c68a5
arturgontijo <arturgontijo@gmail.com>
2020-03-11 12:23:07 -0300

    [orb] SingularityNET Orb 1.2.0

---
+++ scripts/check_daemon.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+export SNETD_VERSION=$1
+export SNETD_CURR_VERSION=""
+if [ "${SNETD_VERSION}" != "" ]; then
+	SNETD_CURR_VERSION=$(ssh -o "StrictHostKeyChecking no" "${SSH_USER}"@"${SSH_HOST}" \
+						docker exec "${PROD_TAG}""${DOCKER_CONTAINER}" snetd version | grep -oP 'version tag: \K(.*)' \
+						|| echo "")
+	if [ "${SNETD_CURR_VERSION}" = "" ] || [ "${SNETD_VERSION}" != "${SNETD_CURR_VERSION}" ]; then
+		ssh -o "StrictHostKeyChecking no" "${SSH_USER}"@"${SSH_HOST}" <<- EOF
+			cd /tmp || true
+			wget https://github.com/singnet/snet-daemon/releases/download/"${SNETD_VERSION}"/snet-daemon-"${SNETD_VERSION}"-linux-amd64.tar.gz
+			tar -xvf snet-daemon-"${SNETD_VERSION}"-linux-amd64.tar.gz
+			docker cp snet-daemon-"${SNETD_VERSION}"-linux-amd64/snetd "${PROD_TAG}""${DOCKER_CONTAINER}":/usr/bin || echo true
+			rm -rf snet-daemon-*
+		EOF
+	fi
+fi
+
+echo 0
